f23053a5ff3a0daaa84b4ed3c52578de238fb92e
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-28 00:23:13 +0530

    new file:   6 Pattern Printing/1_nested_loop.py

---
+++ 6 Pattern Printing/1_nested_loop.py
@@ -0,0 +1,27 @@
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+# * * * * * 
+
+n = 5
+for i in range(0, n):
+    for j in range(0, n):
+        print("* ", end="")
+    print()
+
+print()
+
+# 2 2 2 2 2 
+# 2 2 2 2 2 
+# 2 2 2 2 2 
+# 2 2 2 2 2 
+# 2 2 2 2 2 
+
+n = 5
+for i in range(0, n):
+    for j in range(0, n):
+        print("2 ", end="")
+    print()
+
+
