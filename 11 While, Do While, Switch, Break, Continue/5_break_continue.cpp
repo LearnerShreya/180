8f93d0c0a86f86a97c58334084ddd05b11d307ab
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-28 00:20:55 +0530

    new file:   11 While, Do While, Switch, Break, Continue/5_break_continue.cpp

---
+++ 11 While, Do While, Switch, Break, Continue/5_break_continue.cpp
@@ -0,0 +1,163 @@
+//  BREAK
+
+// 1 2 3 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=1; i<=40; i++)
+    {
+        if(i==4)
+        break;
+        cout<<i<<" ";
+    }
+}
+
+
+// CONTINUE
+
+// 1 2 3 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 
+/* #include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=1; i<=40; i++)
+    {
+        if(i==4)
+        continue;
+        cout<<i<<" ";
+    }
+} */
+
+// Break 
+// 1 
+// 2
+// 3
+
+/* #include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        if(i==4)
+        break;
+        cout<<i<<" "<<endl;
+        i++;
+    }
+} */
+
+
+
+
+
+// continue
+
+// 1 
+// 2 
+// 3 
+/* #include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        if(i==4)
+        continue;
+        cout<<i<<" "<<endl;
+        i++;
+    }
+} */
+
+
+// 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 
+
+/* #include<iostream>
+using namespace std;
+
+int main(){
+    int i=1, n=10, x=20;
+    while(i<x){
+        cout<<i<<" ";
+        i++;
+    }
+} */
+
+// 1 2 3 4 5 6 7 8 9 10 
+
+/* #include<iostream>
+using namespace std;
+
+int main(){
+    int i=1, n=10, x=20;
+    while(i<=n){
+        if(i==x)
+        break;
+        cout<<i<<" ";
+        i++;
+    }
+} */
+
+
+
+// CONTINUE
+
+
+// 1 2 3 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 
+/* #include<iostream>
+using namespace std;
+
+int main()
+{
+    int i;
+    for(i=1; i<=40; i++)
+    {
+        if(i==4)
+        continue;
+        cout<<i<<" ";
+    }
+} */
+
+// 1 ; 2 ; 3 ; 
+/* #include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        if(i==4)
+        continue;
+        cout<<i<<" "<<"; ";
+        i++;
+    }
+} */
+
+
+// 2 ; 3 ; 5 ; 6 ; 7 ; 8 ; 9 ; 10 ; 11 ; 
+
+/* #include<iostream>
+using namespace std;
+
+int main()
+{
+    int i=1;
+    while(i<=10)
+    {
+        i++;
+        if(i==4)
+        continue;
+        cout<<i<<" "<<"; ";
+        
+    }
+} */
