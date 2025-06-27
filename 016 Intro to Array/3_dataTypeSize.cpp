69db3c8e61951cf34896b0b9e30a64288bbb09ee
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-27 21:56:44 +0530

    new file:   016 Intro to Array/3_dataTypeSize.cpp

---
+++ 016 Intro to Array/3_dataTypeSize.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cout<<sizeof(a);
+    cout<<endl;
+
+    bool b;
+    cout<<sizeof(b);
+    cout<<endl;
+
+    char c;
+    cout<<sizeof(c);
+    cout<<endl;
+
+    float d;
+    cout<<sizeof(d);
+    cout<<endl;
+
+    string e;
+    cout<<sizeof(e);
+    cout<<endl;
+
+    long f;
+    cout<<sizeof(f);
+    cout<<endl;
+
+    double g;
+    cout<<sizeof(g);
+    cout<<endl;
+
+    return 0;
+    
+}
