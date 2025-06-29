65cdeaf693a6727c7e810a93f40be994b60c62b5
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:07:20 +0530

    new file:   4 if else , loop/5_pass_fail.cpp

---
+++ 4 if else , loop/5_pass_fail.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int marks;
+    cout<<"Enter your marks: ";
+    cin>>marks;
+    if(marks>33)
+        cout<<"You are pass.";
+    else
+        cout<<"You are fail";
+}
