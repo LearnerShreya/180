7d5a8476c81adb456655a4b2f61a46114969c25b
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:07:07 +0530

    new file:   4 if else , loop/3_package.cpp

---
+++ 4 if else , loop/3_package.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int package;
+    cout<<"Your package is: ";
+    cin>>package;
+    if (package>12)
+    {
+        cout<<"Offer acceptes";
+    }
+    else
+    cout<<"Offer rejected";
+}
