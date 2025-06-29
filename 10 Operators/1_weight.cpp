45e620266030c17e7fd665358e26d53f3c292866
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:03:55 +0530

    new file:   10 Operators/1_weight.cpp

---
+++ 10 Operators/1_weight.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // cout<<2+2<<endl;
+    // cout<<2+2.2<<endl;
+    // cout<<2.2+2.2<<endl;
+
+    cout<<!10<<endl;
+    cout<<!0<<endl;
+    
+}
