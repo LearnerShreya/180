6b2a0d1cde38507294ccb919b35b3707d5620039
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:04:10 +0530

    new file:   10 Operators/3_comparison.cpp

---
+++ 10 Operators/3_comparison.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // int a, b;
+    // cout<<"Enter the value of a: ";
+    // cin>>a;
+    // cout<<"Enter the value of b: ";
+    // cin>>b;
+
+    // {==, <, >, <=, >=, !=}
+    // if(a==b)
+    if(3*9>24<25>13<8*6)
+    cout<<"Yes";
+    else
+    cout<<"NO";
+    
+}
