e7503de199edd7b6ce8f97667f1d5932531b7e60
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:04:35 +0530

    new file:   11 While, Do While, Switch, Break, Continue/0_content.cpp

---
+++ 11 While, Do While, Switch, Break, Continue/0_content.cpp
@@ -0,0 +1,24 @@
+// 00:00 Introduction to loops in c++
+// 01:40 Example with For Loop  & make comparison  
+// 02:40 Syntax of While Loop
+// 03:27 While loop print 1 to n and Debug the code
+// 06:30 Multiplication Table program 
+// 12:05 Write a program to find factors of a Number
+// 18:45 Print all even numbers from 0 to given 'n'  number , Print all Odd  numbers from 0 to given 'n'  number
+// 19:32 Coding part
+// 20:44 Coding part of Multiplication table
+// 21:45 Do While Loop 
+// 24:40 Do while loop example 
+// 26:50 Pre increment and Post increment 
+// 28:30 Sum of 'N" natural numbers
+// 32:00 Code part 
+// 34:25 DisAdvantage of Do-While  
+// 35:28 Concept of Break, Continue 
+// 43:34 Continue statement 
+// 48:43 Code part- break statement 
+// 51:50 Switch statement
+// 58:53 Code part 
+// 01:02:05 switch example
+// 01:03:40 Scope of a Variable
+// 01:13:37 Code Part-- Scope of Variable 
+// 01:16:18 Example 02
