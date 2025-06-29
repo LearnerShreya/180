1a5267e0e6642a9149d367564e6f84f56b6334a9
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:07:00 +0530

    new file:   4 if else , loop/2_typecasting.cpp

---
+++ 4 if else , loop/2_typecasting.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a =10;
+    char c ='d';
+    a=c;
+    cout<<a<<endl;      //typecasting
+
+    int d = 100;
+    char e ='e';
+    e = d;
+    cout<<e;      //typecasting
+}
+
+// char = 1 byte or 8 bit ; therefore maximum value of char is 255
+// 1 byte = 8 bit
