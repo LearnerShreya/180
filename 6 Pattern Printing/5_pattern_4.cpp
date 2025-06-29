c5923e0136f8e5a581b77f6500953d2fa52ab96d
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-30 00:01:32 +0530

    new file:   6 Pattern Printing/5_pattern_4.cpp

---
+++ 6 Pattern Printing/5_pattern_4.cpp
@@ -0,0 +1,47 @@
+// print alphaber
+// row
+// a a a a
+// b b b b
+// c c c c
+// d d d d
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//     char row, col;
+//     for(row='a'; row<='e'; row++)
+//     {
+//         for(col=1; col<6; col++)
+//         {
+//             cout<<row<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+    char row, col;
+    for(row=97; row<=101; row++)
+    {
+        for(col=1; col<6; col++)
+        {
+            cout<<row<<" ";
+        }
+        cout<<endl;
+    }
+
+
+
+//     int row, col;
+//     for(row=1; row<=5; row++)
+//     {
+//         char name = 'a' + row-1;
+//         for(col=1; col<+5; col++)
+//         {
+//             cout<<name<<" ";
+//         }
+//         cout<<endl;
+//     }
+
+}
+
