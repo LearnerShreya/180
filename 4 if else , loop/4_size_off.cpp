ecaf935ed0779c5fe5dc58c571051ce0160f4967
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:07:14 +0530

    new file:   4 if else , loop/4_size_off.cpp

---
+++ 4 if else , loop/4_size_off.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char n ='n';
+    cout<<sizeof(n)<<endl;
+
+    int num = 10;
+    cout<<sizeof(num)<<endl;
+
+    float number =10.10;
+    cout<<sizeof(number)<<endl;
+
+    long nu = 16531;
+    cout<<sizeof(nu)<<endl;
+
+    double numb = 1324.2839497;
+    cout<<sizeof(numb)<<endl;
+}
+
+// char = 1 byte or 8 bit ; therefore maximum value of char is 255
+// 1 byte = 8 bit
