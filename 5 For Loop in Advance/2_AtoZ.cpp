c3b5134e8590b7760fcfbde826afa342cc3f5a4a
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:10:29 +0530

    new file:   5 For Loop in Advance/2_AtoZ.cpp

---
+++ 5 For Loop in Advance/2_AtoZ.cpp
@@ -0,0 +1,15 @@
+// Print Alphabets 'a' to 'z'  
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char i;
+    for(i=65; i<=90; i++)
+    cout<<i<<endl;
+}
+
+// 48 to 57 i.e. 0 to 9
+// 65 to 90 i.e. A to Z
+// 97 to 122 i.e. a to z
