321679ccca999d2ecdae3989c974e16928b6cf71
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:13:12 +0530

    new file:   5 For Loop in Advance/4_number_reverse.cpp

---
+++ 5 For Loop in Advance/4_number_reverse.cpp
@@ -0,0 +1,14 @@
+// Print numbers in Reverse order
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+    cout<<"Enter number: ";
+    cin>>n;
+
+    for(i=n; i>=1; i--)
+    cout<<i<<" ";
+}
