99290f3b04328cc5a36935ac5a3b8d9cfd8edb8e
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:13:07 +0530

    new file:   5 For Loop in Advance/3_z_a.cpp

---
+++ 5 For Loop in Advance/3_z_a.cpp
@@ -0,0 +1,21 @@
+// Print alphabet in Reverse order
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     char i;
+//     for(i='z'; i>='a'; i--)
+//     cout<<i<<" ";
+// }
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char i;
+    for(i=122; i>=97; i--)
+    cout<<i<<" ";
+}
