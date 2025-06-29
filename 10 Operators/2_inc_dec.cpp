60340486edaae646a3579accbe008b880d654662
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:04:03 +0530

    new file:   10 Operators/2_inc_dec.cpp

---
+++ 10 Operators/2_inc_dec.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a=10;
+    cout<<a++<<endl;
+    cout<<a<<endl<<endl;
+
+    int b = 10;
+    cout<<++b<<endl;
+    cout<<b;
+    cout<<endl<<endl;
+
+    int c=10;
+    cout<<c--<<endl;
+    cout<<c<<endl<<endl;
+
+    int d = 10;
+    cout<<--d<<endl;
+    cout<<d;
+}
