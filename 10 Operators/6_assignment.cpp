be20b25cea3931aa17d54c3f71e1002869b7ec05
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:04:29 +0530

    new file:   10 Operators/6_assignment.cpp

---
+++ 10 Operators/6_assignment.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // Assignment Operator
+
+    int a = 10;
+    a+=15; // a = a + 15;
+    cout<<"Answer is: ";
+    cout<<a;
+    cout<<endl;
+
+    int b = 10;
+    b-=15; // b = b - 15;
+    cout<<"Answer is: ";
+    cout<<b;
+    cout<<endl;
+
+    int c = 10;
+    c*=15; // c = c * 15;
+    cout<<"Answer is: ";
+    cout<<c;
+    cout<<endl;
+
+    int d = 10;
+    d/=15; // d = d / 15;
+    cout<<"Answer is: ";
+    cout<<d;
+    cout<<endl;
+
+    int e = 10;
+    e%=15; // e = e % 15;
+    cout<<"Answer is: ";
+    cout<<e;
+    cout<<endl;
+    
+}
