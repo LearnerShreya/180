6572bf971e589738f69994fa82d74659768e1835
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:14:11 +0530

    new file:   6 Pattern Printing/1_nested_loop.cpp

---
+++ 6 Pattern Printing/1_nested_loop.cpp
@@ -0,0 +1,48 @@
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
+// * * * * * 
+
+// #include<iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int row, col;
+//     for(row=1; row<=5; row++)
+//     {
+//         for(col=1; col<=5; col++)
+//         {
+//             cout<<"* ";
+
+//         } 
+//         cout<<endl;
+//    }
+//     return 0;
+// }
+
+
+// 2 2 2 2 2 
+// 2 2 2 2 2 
+// 2 2 2 2 2 
+// 2 2 2 2 2 
+// 2 2 2 2 2 
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    for(row=1; row<=5; row++)
+    {
+        for(col=1; col<=5; col++)
+        {
+            cout<<"2 ";
+
+        } 
+        cout<<endl;
+   }
+    return 0;
+}
