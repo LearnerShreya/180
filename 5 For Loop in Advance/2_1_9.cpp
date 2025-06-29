dc71007345a3a0c0f6f49cd3ba13ddeadf142e55
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:10:18 +0530

    new file:   5 For Loop in Advance/2_1_9.cpp

---
+++ 5 For Loop in Advance/2_1_9.cpp
@@ -0,0 +1,16 @@
+// Print digit 0-9 using ascii code
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char i;
+    for(i=48; i<=57; i++)
+    cout<<i<<endl;
+}
+
+
+// 48 to 57 i.e. 0 to 9
+// 65 to 90 i.e. A to Z
+// 97 to 122 i.e. a to z
