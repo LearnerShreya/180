35f50ba4c075cc36e710fd949800f439deb01be9
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:07:26 +0530

    new file:   4 if else , loop/6_greater.cpp

---
+++ 4 if else , loop/6_greater.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout<<"Enter your first number: ";
+    cin>>a;
+    cout<<"Enter your second number: ";
+    cin>>b;
+
+    if(a>b){
+        cout<<"a is greater then b";
+    }
+    else{
+        cout<<"b is greater than a";
+    }
+    return 0;
+}
