5b13e0a0f87a0c0fd0a6b2fd6dafaf91d59c3b3a
Shreya Singh <shreya.official.2.25@gmail.com>
2025-06-29 12:04:23 +0530

    new file:   10 Operators/5_bitwise.cpp

---
+++ 10 Operators/5_bitwise.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // Bitwise Operator
+
+    // & Operator
+    int ans = 2&3;
+    cout<<"My answer is: "<<ans<<endl;
+    // | Operator
+    int ant = 2|3;
+    cout<<"My answer is: "<<ant<<endl;
+    // ^ Operator
+    int anu = 2^3;
+    cout<<"My answer is: "<<anu<<endl;
+    // >> Operator
+    int anv = 10>>3;
+    cout<<"My answer is: "<<anv<<endl;
+    // << Operator
+    int anw = 2<<3;
+    cout<<"My answer is: "<<anw<<endl;
+    // ~ Operator
+    int anx = ~5;
+    cout<<"My answer is: "<<anx<<endl;
+    // ~ Operator
+    int any = ~-15;
+    cout<<"My answer is: "<<any<<endl;
+    
+}
