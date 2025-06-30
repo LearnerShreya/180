a99700e7ae27bc102213ea41ecf44bb4756b3346
Shreya Singh <shreya.official.2.25@gmail.com>
2025-07-01 00:21:54 +0530

    modified:   6 Pattern Printing/1_nested_loop.py

--- 6 Pattern Printing/1_nested_loop.py
+++ 6 Pattern Printing/1_nested_loop.py
@@ -5,11 +5,17 @@
 # * * * * * 
 
 n = 5
-for i in range(0, n):
+for i in range(0, n):        # works, but i is unused
     for j in range(0, n):
         print("* ", end="")
     print()
+print()
 
+# Optimized version of the above pattern:
+n = 5
+row = "* " * n
+for _ in range(n):
+    print(row)
 print()
 
 # 2 2 2 2 2 
@@ -23,5 +29,3 @@ for i in range(0, n):
     for j in range(0, n):
         print("2 ", end="")
     print()
-
-
