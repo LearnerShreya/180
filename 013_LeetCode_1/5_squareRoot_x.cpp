7ca474fb3e366223fc672f249bbccaf20f784cd1
Shreya Singh <shreya.official.2.25@gmail.com>
2025-07-09 11:30:44 +0530

    modified:   013_LeetCode_1/5_squareRoot_x.cpp

--- 013_LeetCode_1/5_squareRoot_x.cpp
+++ 013_LeetCode_1/5_squareRoot_x.cpp
@@ -1,19 +1,43 @@
-// Square root of  x  
-
 #include<iostream>
 using namespace std;
 
-int main()
-{
-    int num, n, sqt, sol;
-    cout<<"Enter number: ";
-    cin>>num;
+int main() {
+    int num;
+    cout << "Enter number: ";
+    cin >> num;
 
-    for(n=1; n*n<=num; n++)
-    {
-        sqt=n*n;
-        sol=sqt;
+
+    // 1. Simple For Loop (Safest & Cleanest)
+    // This stores correct value inside loop, no need to subtract 1
+
+
+    int sol = 0;
+    for(int i = 1; i * i <= num; i++) {
+        sol = i;
     }
-    if(sol<=num)
-    cout<<n-1 ;
+    cout <<sol << endl;
+
+
+    // 2. While Loop (Needs subtract 1)
+    // This loop goes 1 step too far, needs -1 after loop
+
+
+    int n = 0;
+    while(n * n <= num) {
+        n++;
+    }
+    cout << n - 1 << endl;
+
+
+    // 3. For Loop 
+    // This loop also goes 1 step too far, needs -1 after loop
+
+
+    int m,p;
+    for(m = 1; m * m <= num; m++) {
+        // Empty body, just finding limit
+    }
+    cout << p - 1 << endl;
+
+    return 0;
 }
